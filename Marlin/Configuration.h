10d611e20b7fcd91f862712d04a4e02d156862d9
chanhpomme <chanhpomme@gmail.com>
2019-05-10 12:09:15 +0200

    Configuration.h : LV8729 on the Z axis

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -559,7 +559,7 @@
 //#define X2_DRIVER_TYPE A4988
 //#define Y2_DRIVER_TYPE A4988
 //#define Z2_DRIVER_TYPE A4988
-#define E0_DRIVER_TYPE TMC2208
+#define E0_DRIVER_TYPE LV8729
 //#define E1_DRIVER_TYPE A4988
 //#define E2_DRIVER_TYPE A4988
 //#define E3_DRIVER_TYPE A4988
@@ -956,7 +956,7 @@
 // Invert the stepper direction. Change (or reverse the motor connector) if an axis goes the wrong way.
 #define INVERT_X_DIR false
 #define INVERT_Y_DIR false
-#define INVERT_Z_DIR true
+#define INVERT_Z_DIR false
 
 // @section extruder
 
